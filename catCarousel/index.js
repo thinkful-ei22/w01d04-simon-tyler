3cca2d6d47ab5a340a5c9c7987a177aeeaf000f8
DivaGodiva <33299951+DivaGodiva@users.noreply.github.com>
2018-06-14 16:50:06 -0700

    Update index.js

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -6,3 +6,52 @@ $(function(){
     $('.hero img').attr('src', picture);
   });
 });
+
+
+
+
+
+/*eslint-env jquery*/
+const fizzBuzz = function(num){
+  if (num % 15 === 0){
+    return 'fizzbuzz';
+  }
+  if (num % 5 === 0){
+    return 'buzz';
+  }
+  if (num % 35 === 0){
+    return 'fizz';
+  }
+  else {
+    return num;
+  }
+};
+
+const fizzBuzzhtml = function(n){
+  let fizzClass = '';
+  if(typeof n === 'string'){
+    fizzClass = n;
+  }
+  return `<div class='fizz-buzz-items'> ${fizzClass} <span> ${n} </span> </div>`;
+};
+
+const handleSubmit = function(){
+  $('#number-chooser').on('submit', event => {
+    event.preventDefault();
+    const userInput = $('#number-choice').val();
+    $('#number-choice').val('');
+    const fizzBuzzItems = [];
+    for (let i = 1; i <= userInput; i++)
+    {
+      fizzBuzzItems.push(fizzBuzz[i]);
+    }
+    const html = fizzBuzzItems.map(item => fizzBuzzhtml(item));
+    $('.js-results').append(html);
+  });
+
+};
+function main() {
+  handleSubmit();
+}
+
+main();
