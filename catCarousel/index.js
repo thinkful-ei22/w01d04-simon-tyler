9323976e117f63518cdcf432f620c3912da5f064
Tyler Warrick <tcw5038@gmail.com>
2018-06-14 09:39:38 -0700

    Finished first exercise

---
+++ catCarousel/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$(function(){
+    $('.thumbnail').click(function(event){
+        let picture = $(this).find('img').attr('src');
+       $('.hero img').attr('src', picture);
+    });
+});
