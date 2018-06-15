ec3d6de7cfe3a9397dae2458c682801879cf3b7d
Simon Chae <s13chae@gmail.com>
2018-06-14 17:04:10 -0700

    fixed the thing

--- catCarousel/catCarousel.js
+++ catCarousel/catCarousel.js
@@ -1,53 +1,12 @@
 'use strict';
 /*eslint-env jquery*/
 
-const userInput = $('.js-shopping-list-entry');
-
-console.log(userInput);
-
-const addItemToHTML = function(submitFood) {
-  return `<li>
-  <span class="shopping-item">${submitFood}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle">
-      <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete">
-      <span class="button-label">delete</span>
-    </button>
-  </div>
-  </li>`;
-};
-
-const addUserInput = function(foodItem) {
-  return addItemToHTML(foodItem);
-};
-
-const submitFunction = function() {
-  $('#js-shopping-list-form').submit(function(event) {
-    event.preventDefault();
-    const addedHtml = addUserInput(userInput.val());
-    $('ul').append(addedHtml);
-    userInput.val('');
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(this).find('img').attr('src');  
+    const imgAlt = $(this).find('img').attr('alt');
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
   });
-};
-
-const checkOffItem = function() {
-  $('body').on('click', '.shopping-item-toggle', function(event) { 
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-  });
-};
-
-const deleteItem = function() {
-  $('body').on('click', '.shopping-item-delete', function(event) {
-    $(this).closest('li').remove();
-  });
-};
-
-function main() {
-  submitFunction();
-  checkOffItem();
-  deleteItem();
 }
 
-main();
+$(handleThumbnailClicks);
